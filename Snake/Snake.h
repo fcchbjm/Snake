c62890fc4cf543e9c60d1248e0f1c93b14f31eec
fcchbjm <2748195351@qq.com>
2024-04-18 10:47:32 +0800

    upload

--- Snake/Snake.h
+++ Snake/Snake.h
@@ -5,6 +5,14 @@
 #include <windows.h>
 #include <stdbool.h>
 #include <locale.h>
+#include <time.h>
+
+#define POS_X 24
+#define POS_Y 5
+
+#define WALL L'□'
+#define BODY L'●'
+#define FOOD L'★'
 
 //蛇的方向
 enum DIRECTION
@@ -41,7 +49,7 @@ typedef struct SnakeNode
 typedef struct Snake
 {
 	pSnakeNode _pSnake;//指向蛇头的指针
-	pSnakeNode _pfood;//指向食物节点的指针
+	pSnakeNode _pFood;//指向食物节点的指针
 	enum DIRECTION _dir;//蛇的方向
 	enum GAME_STATUS _status;//蛇的状态
 	int _food_weight;//一个食物的分数
@@ -51,6 +59,9 @@ typedef struct Snake
 
 //函数的声明
 
+//定位光标位置
+void SetPos(short x, short y);
+
 //游戏的初始化
 void GameStart(pSnake ps);
 
@@ -59,3 +70,33 @@ void WelcomeToGame();
 
 //创建地图
 void CreateMap();
+
+//初始化蛇身
+void InitSnake(pSnake ps);
+
+//创建食物
+void CreateFood(pSnake ps);
+
+//游戏运行的逻辑
+void GameRun(pSnake ps);
+
+//蛇的移动-走一步
+void SnakeMove(pSnake ps);
+
+//判断下一个坐标是否是食物
+int NextIsFood(pSnakeNode pn, pSnake ps);
+
+//下一个位置是食物，就吃掉食物
+void EatFood(pSnakeNode pn, pSnake ps);
+
+//下一个位置不是食物
+void NoFood(pSnakeNode pn, pSnake ps);
+
+//检测蛇是否撞墙
+void KillByWall(pSnake ps);
+
+//检测蛇是否撞到自己
+void KillBySelf(pSnake ps);
+
+//游戏善后的工作
+void GameEnd(pSnake ps);
--- Snake/test.c
+++ Snake/test.c
@@ -1,37 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-
-
 #include "Snake.h"
 
-void test(void)
+//完成的是游戏的测试逻辑
+void test()
 {
-	//创建贪吃蛇
+	int ch = 0;
+	do
+	{
+		system("cls");
+		//创建贪吃蛇
+		Snake snake = { 0 };
+		//初始化游戏
+		//1. 打印欢迎界面
+		//2. 功能介绍
+		//3. 绘制地图
+		//4. 创建蛇
+		//5. 创建食物
+		//6. 设置游戏的相关信息
+		GameStart(&snake);
 
-	//pSnakeNode pSnake = NULL;
-	Snake snake = { 0 };
+		//运行游戏
+		GameRun(&snake);
+		//结束游戏 - 善后工作
+		GameEnd(&snake);
+		SetPos(20, 15);
+		wprintf(L"再来一局吗?(Y/N):");
+		ch = getchar();
+		while (getchar() != '\n');
 
-	//初始化游戏
-	//1. 打印环境界面
-	//2. 功能介绍
-	//3. 绘制地图
-	//4. 创建蛇
-	//5. 创建食物
-	//6. 设置游戏的相关信息
-	GameStart(&snake);
-
-	//运行游戏
-	//GameRun();
-
-	//结束游戏 - 善后工作
-	//GameEend();
+	} while (ch == 'Y' || ch == 'y');
+	SetPos(0, 27);
 }
 
 int main()
 {
-	//设置本地环境
+	//设置适配本地环境
 	setlocale(LC_ALL, "");
-
+	srand((unsigned int)time(NULL));
 	test();
 
 	return 0;
